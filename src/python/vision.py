65cbf3c4fc874207520ce14d916e150c70ebde4a
Het-Muinsie <tijldelang@gmail.com>
2024-10-22 15:20:40 +0200

    added hi

--- src/python/vision.py
+++ src/python/vision.py
@@ -12,9 +12,6 @@ _MIN_CONFIDENCE: float = 0.8
 _model = YOLO("./yolo_models/640x480_100epoch.pt")
 _webcam = Webcam(src=0, w=_FRAME_WIDTH, h=_FRAME_HEIGHT)
 
-#hallo mensen
-#hi
-#hi2
 
 def draw_cross(
         frame: np.ndarray,
