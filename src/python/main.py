4ce8880b826b43a62844e0113b7ef4b4675212e0
Joost <joostjv@outlook.com>
2024-11-02 23:43:30 +0100

    Moved old files to respective testing folders. Wrote new python files for vision and arduino communication. Preliminary testing suggests it seems to work well

--- src/python/main.py
+++ src/python/main.py
@@ -1,21 +1,62 @@
+import cv2
 from serial import Serial
 from time import sleep
+from vision import get_frame_data, Target
+
 
 COM_PORT: str = "COM4"
-BAUD_RATE: str = "600"
+BAUD_RATE: int = 9600
 
 
-def send_message(message: str, serial: Serial) -> None:
-    embed = f"{message}".encode(encoding="ascii")
-    print(embed)
-    serial.write(embed)
+def send_command(arduino: Serial, *, angle: tuple[int, int] = None, home: bool = False, trigger: bool = False) -> None:
+    
+    if trigger:
+        arduino.write("trigger\n".encode())
+    
+    elif home:
+        arduino.write("home\n".encode())
+    
+    elif angle is not None:
+        angle_command = "r"
+        dx, dy = angle
+        angle_command += f"{dx},{dy}\n"
+        print(angle_command)
+        arduino.write(angle_command.encode())
 
 
-def main() -> None:
+def calculate_rotation(dx: int, dy: int) -> tuple[int, int]:
+    return dx, dy
+
+
+def main():
+    
+    # establish arduino connection and give time it time to initialize
+    # running in context manager such that the connection gets closed properly if an exception is raised during runtime
     with Serial(port=COM_PORT, baudrate=BAUD_RATE) as arduino:
-        arduino.flush()
-        send_message(message="t", serial=arduino)
-
-
+        sleep(2)
+        
+        running = True
+        
+        while running:
+            
+            # wait until arduino is ready for next command
+            if not arduino.readline().strip() == b"ready":
+                continue
+            
+            frame_data = get_frame_data()
+            
+            if frame_data.targets:
+                dx = frame_data.targets[0].center_x - frame_data.frame_width // 2
+                dy = frame_data.targets[0].center_y - frame_data.frame_height // 2
+                angles = calculate_rotation(dx=dx, dy=dy)
+                send_command(arduino=arduino, angle=angles)
+            
+            cv2.imshow(winname="vision", mat=frame_data.annotated_frame)
+            
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                running = False
+            
+        
+# only call main() main when this file is ran directly as opposed to imported
 if __name__ == "__main__":
     main()
--- src/python/vision.py
+++ src/python/vision.py
@@ -1,101 +1,110 @@
+from dataclasses import dataclass
+import numpy as np
+from webcam import Webcam
 import cv2
 from ultralytics import YOLO
-from webcam import Webcam
-import numpy as np
 
-_FRAME_WIDTH: int = 640
-_FRAME_HEIGHT: int = 480
+
+_VIDEO_SOURCE: int = 0
+_VIDEO_WIDTH: int = 640
+_VIDEO_HEIGHT: int = 480
 
 _PERSON_CLASS_ID: int = 0
 _MIN_CONFIDENCE: float = 0.8
 
-_model = YOLO("./yolo_models/640x480_100epoch.pt")
-_webcam = Webcam(src=0, w=_FRAME_WIDTH, h=_FRAME_HEIGHT)
+_model = YOLO("640px_100epoch.pt")
+_camera: Webcam = Webcam(src=_VIDEO_SOURCE, w=_VIDEO_WIDTH, h=_VIDEO_HEIGHT)
 
 
-def draw_cross(
-        frame: np.ndarray,
-        center: tuple[int, int],
-        size: int = 20,
-        color: tuple[float, float, float] = (0., 0., 0.),
-        thickness: int = 2
-) -> np.ndarray:
-    add_vert = cv2.line(
-        img=frame,
-        pt1=(center[0], center[1] - size // 2),
-        pt2=(center[0], center[1] + size // 2),
-        color=color,
-        thickness=thickness
-    )
-    add_hor = cv2.line(
-        img=add_vert,
-        pt1=(center[0] - size // 2, center[1]),
-        pt2=(center[0] + size // 2, center[1]),
-        color=color,
-        thickness=thickness
-    )
-    return add_hor
-
-
-def main():
+class Target:
+    # __slots__ makes class attributes static, which increases performance and reduces chances of silent errors
+    __slots__ = [
+        "corner1_x",
+        "corner1_y",
+        "corner2_x",
+        "corner2_y",
+        "track_id",
+        "confidence",
+        "classification",
+        "center_x",
+        "center_y",
+        "size_x",
+        "size_y"
+    ]
     
-    # initial model training
-    # train_results = _model.train(
-    #     data="coco8.yaml",
-    #     epochs=100,
-    #     imgsz=_FRAME_WIDTH,
-    #     device="cpu"
-    # )
-    #
-    # metrics = _model.val()
+    def __init__(
+            self,
+            c1x: float,
+            c1y: float,
+            c2x: float,
+            c2y: float,
+            track_id: float,
+            confidence: float,
+            classification: float
+    ) -> None:
+        self.corner1_x: int = round(c1x)
+        self.corner1_y: int = round(c1y)
+        self.corner2_x: int = round(c2x)
+        self.corner2_y: int = round(c2y)
+        self.track_id: int | None = int(track_id) if track_id is not None else None
+        self.confidence: float = confidence
+        self.classification: int = int(classification)
+        self.center_x: int = (self.corner1_x + self.corner2_x) // 2
+        self.center_y: int = (self.corner1_y + self.corner2_y) // 2
+        self.size_x: int = abs(self.corner1_x - self.center_x)
+        self.size_y: int = abs(self.corner1_y - self.center_y)
+
+
+@dataclass(slots=True)
+class FrameData:
+    targets: list[Target]
+    frame_width: int
+    frame_height: int
+    annotated_frame: np.ndarray
+
+
+def get_frame_data() -> FrameData:
     
-    for frame in _webcam:
-        # swap colour channels because cv2 works with BGR instead of RGB values for some reason
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        
-        # track object and copy tracking data to cpu memory
-        results = _model.track(frame, persist=True)[0].cpu()
-        
-        # create annotated frame with detection boxes, labels and confidence values
-        annotated_frame = results.plot()
-        
+    # get frame from camera and convert to BGR because cv2 works with BGR for some reason
+    frame = _camera.read_next_frame()
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    
+    # use machine learning model to classify objects in frame and save to cpu memory
+    results = _model.track(frame, persist=True)[0].cpu()
+    
+    raw_targets = []
+    for box in results.boxes.data.tolist():
         # get all the relevant data of all boxes stored in a list of lists containing
         # [corner 1 x, corner 1 y, corner 2 x, corner 2 y, track id, confidence, classification]
-        boxes = results.boxes.data.tolist()
         
-        # go over all the boxes, check if they are classified as person and put a crosshair if the confidence is greater
-        # than specified by _MIN_CONFIDENCE
-        for box in boxes:
-            c1x, c1y, c2x, c2y, track_id, conf, cls = tuple(box)
-            box_center = int((c1x + c2x) / 2), int((c1y + c2y) / 2)
-            
-            if int(cls) == _PERSON_CLASS_ID and conf >= _MIN_CONFIDENCE:
-                annotated_frame = draw_cross(frame=annotated_frame, center=box_center, color=(255., 0., 0.))
-                annotated_frame = cv2.putText(
-                    img=annotated_frame,
-                    text=f"dx = {box_center[0] - _FRAME_WIDTH // 2}; dy {box_center[1] - _FRAME_HEIGHT // 2}",
-                    org=(10, _FRAME_HEIGHT - 10),
-                    fontFace=cv2.FONT_HERSHEY_PLAIN,
-                    fontScale=1,
-                    color=(0., 255., 0.),
-                    thickness=1,
-                    lineType=cv2.LINE_AA,
-                    bottomLeftOrigin=False,
-                )
+        # sometimes the tracking system won't return an ID, insert None if this is the case
+        if len(box) == 6:
+            box.insert(4, None)
         
-        # draw center crosshair
-        annotated_frame = draw_cross(
-            frame=annotated_frame,
-            center=(_FRAME_WIDTH // 2, _FRAME_HEIGHT // 2),
-            color=(0., 255., 0.)
-        )
-        
-        cv2.imshow("Webcam Frame", annotated_frame)
-        
-        # exit loop if user presses q
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
+        # if the object is classified as a person and the confidence is above the minimum threshold
+        if int(box[6]) == _PERSON_CLASS_ID and box[5] > _MIN_CONFIDENCE:
+            raw_targets.append(box)
     
+    annotated_frame = results.plot()
+    
+    return FrameData(
+        targets=[Target(*target) for target in raw_targets],
+        frame_width=_VIDEO_WIDTH,
+        frame_height=_VIDEO_HEIGHT,
+        annotated_frame=annotated_frame
+    )
 
+
+def main() -> None:
+    # this function is just for testing and is not used in prod
+    while True:
+        try:
+            get_frame_data()
+            
+        except KeyboardInterrupt:
+            break
+
+
+# only call main() main when this file is ran directly as opposed to imported
 if __name__ == "__main__":
     main()
