3622a02eaa2aabfe81ca0acc7d73cdbf02335705
Joost <joostjv@outlook.com>
2024-11-02 17:59:35 +0100

    Added working servo control code for arduino. Control code now accepts degree changes rather than pixel changes

---
+++ src/arduino/arduino_code.ino
@@ -0,0 +1,134 @@
+#include <Servo.h>
+
+/*
+ * Configuration of global constants
+ */
+constexpr int hor_servo_home = 0,
+              hor_servo_min_angle = -90,
+              hor_servo_max_angle = 90,
+              hor_servo_pin = 3,
+
+              ver_servo_home = 0,
+              ver_servo_min_angle = -90,
+              ver_servo_max_angle = 90,
+              ver_servo_pin = 4,
+
+              tri_servo_cold = 25,
+              tri_servo_hot = 80,
+              tri_servo_pin = 5,
+              tri_servo_delay = 1000,
+
+              baud_rate = 9600,
+              serial_timeout = 100;
+
+
+/*
+ * Initialisation of global variables
+ */
+int hor_servo_target_angle,
+    ver_servo_target_angle;
+
+bool triggered;
+
+Servo hor_servo,
+      ver_servo,
+      tri_servo;
+
+String message;
+
+
+/*
+ * Operational code
+ */
+int clamp_angle(int current, int adjustment, int min, int max){
+  // Make sure the new target angle does not exceed allowable values
+  if (current + adjustment > max) {
+    return max;
+  }
+  else if (current + adjustment < min) {
+    return min;
+  }
+  return (current + adjustment);
+}
+
+void adjust_rotation(char axis, int adjustment) {
+  // Write a new target angle for a given axis
+  switch(axis){
+    case 'x':
+      hor_servo_target_angle = clamp_angle(
+        hor_servo_target_angle,
+        adjustment,
+        hor_servo_min_angle,
+        hor_servo_max_angle
+      );
+      break;
+    case 'y':
+      ver_servo_target_angle = clamp_angle(
+        ver_servo_target_angle,
+        adjustment,
+        ver_servo_min_angle,
+        ver_servo_max_angle
+      );
+      break;
+  }
+}
+
+void message_parser() {
+  // rotation commands are of the format r(+|-)xx,(+|-)yy
+  if (message[0] == 'r') {
+    int separator = message.indexOf(',');
+
+    String x_str = message.substring(1, separator),
+           y_str = message.substring(separator + 1);
+
+    int dx = x_str.toInt(),
+        dy = y_str.toInt();
+
+    adjust_rotation('x', dx);
+    adjust_rotation('y', dy);
+
+  }
+  // trigger commands start with the letter t
+  else if (message[0] == 't') {
+    triggered = true;
+  }
+}
+
+void setup() {
+  Serial.begin(baud_rate);
+  Serial.setTimeout(serial_timeout);
+
+  hor_servo.attach(hor_servo_pin);
+  ver_servo.attach(ver_servo_pin);
+  tri_servo.attach(tri_servo_pin);
+  hor_servo.write(hor_servo_home);
+  ver_servo.write(ver_servo_home);
+  tri_servo.write(tri_servo_cold);
+
+  hor_servo_target_angle = hor_servo_home;
+  ver_servo_target_angle = ver_servo_home;
+  triggered = false;
+
+  Serial.println(hor_servo_target_angle);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    message = Serial.readStringUntil('\n');
+    
+    message_parser();   // interpret message and change appropriate globals
+
+    if (triggered) {    // pull trigger
+      tri_servo.write(tri_servo_hot);
+      delay(tri_servo_delay);
+      tri_servo.write(tri_servo_cold);
+      triggered = false;
+    }
+
+    else {    // 
+      hor_servo.write(hor_servo_target_angle);
+      ver_servo.write(ver_servo_target_angle);
+    }
+    Serial.println("ready");     // let PC know the arduino is ready for next command
+  }
+}
